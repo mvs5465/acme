8b2945ac36711a721ef98cab0cafac721db014df
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-11-21 12:28:52 -0500

    Messing with templating and directives

--- acme.py
+++ acme.py
@@ -6,6 +6,12 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def show_index():
-  return render_template('index.html', status='good')
+  return render_template('index.html', title='ACME', status='in progress')
 
+@app.route('/programmer')
+def programmer():
+  return render_template('index.html', title='Programmer', content='programmer.html')
 
+@app.route('/designer')
+def designer():
+  return render_template('index.html', title='Designer', content='designer.html')
