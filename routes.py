950882d242acdcdf5f5271c0fe77b83732206ca9
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-11-30 10:15:37 -0500

    Added routes.pyc and run.pyc to gitignore

--- routes.py
+++ routes.py
@@ -34,15 +34,6 @@ def show_entries():
     entries = cur.fetchall()
     return render_template('layout.html', content='modules/data.html', status='running', entries=entries)
 
-"""
-@app.route('/db/custom')
-def custom_query():
-    db = get_db()
-    cur = db.execute('SELECT * FROM salaries ORDER BY TotalPay DESC LIMIT 20;')
-    entries = cur.fetchall()
-    return render_template('layout.html', content='modules/data.html', status='good', entries=entries)
-"""
-
 @app.route('/db/custom')
 def custom_query():
     entries = []
@@ -51,4 +42,28 @@ def custom_query():
     cur = db.execute(query)
     entries = cur.fetchall()
     keys = cur.description
+    return render_template('layout.html', title='Database', content='modules/data.html', status='good', entries=entries, query=query, keys=keys)
+
+@app.route('/db/builder')
+def build_query():
+    
+    # Define variables
+    query = ""
+    entries = []
+    keys = []
+    
+    # Extract query info from GET request
+    columns = request.args.getlist('fields')
+    years = request.args.getlist('years')
+    
+    # Construct query
+    query = "SELECT " + columns[0] + " FROM salaries LIMIT 100;"
+    
+    # Execute query
+    db = get_db()
+    cur = db.execute(query)
+    entries = cur.fetchall()
+    keys = cur.description
+    
+    # Return template along with variables
     return render_template('layout.html', title='Database', content='modules/data.html', status='good', entries=entries, query=query, keys=keys)
